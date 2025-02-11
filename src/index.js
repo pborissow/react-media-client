39577b102d2a9b52961ce7d466df7caf2cc7681a
pborissow <email@pborissow.dev>
2025-02-11 12:44:07 -0500

    Style updates

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import {Application} from './Application';
 
-var parent = document.getElementById('root');
+var parent = document.body;
 ReactDOM.createRoot(parent).render(<Application/>);
